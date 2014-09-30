5ce755fea481f209e4a4d7863a070f592579fadc
Chadwick Flores <chadwickflores@gmail.com>
2014-09-29 23:25:00 -0500

    chapter 7

---
+++ example.rb
@@ -0,0 +1,12 @@
+if name == name.capitalize
+	puts 'Please take a seat, ' + name + '.'
+else
+	puts name + '? You mean ' + name.capitalize + ', right?'
+	puts 'Don\'t you even know how to spell your name?'
+	reply = gets.chomp
+	if reply.downcase == 'yes'
+		puts 'Hmmph! Well, sit down!'
+	else
+		puts 'GET OUT!!'
+	end
+end
